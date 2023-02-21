e8b307f716c1d966b915b3285959f5d9b083f1a5
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2023-02-21 19:31:08 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
+function handleCredentialResponse(response) {
+  // decodeJwtResponse() is a custom function defined by you
+  // to decode the credential response.
+  const responsePayload = decodeJwtResponse(response.credential);
+  let fullname = document.getElementById("fullname")
+  let first = document.getElementById("first")
+  let last = document.getElementById("last")
+  let mail = document.getElementById("email")
+  let photo = document.getElementById("photo")
+  let id_num = document.getElementById("id_num")
+  let sign = document.getElementById("sign");
+  let out = document.getElementById("out");
+  let info = document.getElementById("info");
+  let photo_link = responsePayload.picture;
 
-    // let name = document.getElementsByClassName("name")
-    // let id = document.getElementsByClassName("id")
-    // let mail = document.getElementsByClassName("mail")
-    // let img = document.getElementsByClassName("img")
+  info.classList.remove("d-none");
+  fullname.innerHTML = `${responsePayload.name}`;
+  photo.src = photo_link;
+  first.innerHTML = `${responsePayload.given_name}`;
+  last.innerHTML = `${responsePayload.family_name}`;
+  mail.innerHTML = `${responsePayload.email}`;
+  id_num.innerHTML = `${responsePayload.sub}`;
 
-    // name.innerHTML= `Name: ${profile.getName()}`
-    // id.innerHTML= `ID: ${profile.getId()}`
-    // mail.innerHTML= `Email: ${profile.getEmail()}`
+  sign.classList.add("d-none");
+  out.classList.remove("d-none");
 
-    // img.src= `${profile.get()}`
+}
 
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+function decodeJwtResponse(data) {
+  let tokens = data.split(".");
+  return JSON.parse(atob(tokens[1]))
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
+out.addEventListener("click", signOut)
 
 
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
